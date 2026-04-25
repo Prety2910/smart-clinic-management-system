a0399cfda32c6194e0025961123696b7b697afd9
Prety2910 <sharmaprety99@gmail.com>
2026-04-25 20:42:37 +0530

    Create Appointment.java

---
+++ app/src/main/java/com/project/back_end/models/app/src/main/java/com/project/back_end/models/Appointment.java
@@ -0,0 +1,20 @@
+package com.project.back_end.models;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Appointment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Doctor doctor;
+
+    @ManyToOne
+    private Patient patient;
+
+    private LocalDateTime appointmentTime;
+}
