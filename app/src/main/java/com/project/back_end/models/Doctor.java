7dc79af211272fc154fa4d0040df638dd87d31d5
Prety2910 <sharmaprety99@gmail.com>
2026-04-25 20:42:16 +0530

    Create Doctor.java

---
+++ app/src/main/java/com/project/back_end/models/Doctor.java
@@ -0,0 +1,18 @@
+package com.project.back_end.models;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String speciality;
+
+    @ElementCollection
+    private List<String> availableTimes;
+}
